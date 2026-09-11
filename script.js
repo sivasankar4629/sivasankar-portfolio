fca1d4f8076ffb0bce554efc8275190eb90917ae
sivasankar4629 <92851086+sivasankar4629@users.noreply.github.com>
2026-09-12 04:32:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  toggle.textContent = document.body.classList.contains("light") ? "☀" : "☾";
+});
